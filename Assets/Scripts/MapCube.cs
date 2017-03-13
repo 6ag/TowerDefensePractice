587a9463cedae68c0ba2263e305efe7784d1ef2d
zhoujianfeng <admin@6ag.cn>
2017-03-13 16:15:21 +0800

    添加创建炮塔特效

--- Assets/Scripts/MapCube.cs
+++ Assets/Scripts/MapCube.cs
@@ -7,11 +7,15 @@ public class MapCube : MonoBehaviour {
 
 	[HideInInspector] // [HideInInspector]可以隐藏这个public属性显示在inspector面板
 	public GameObject turretGo; // 当前cube下的炮塔，如果为空，则表示当前位置没有炮塔
+	public GameObject buildEffect;
 
 	// 构建炮塔
 	public void BuildTurret(GameObject turretPrefab) 
 	{
 		// 实例化炮塔
 		turretGo = GameObject.Instantiate(turretPrefab, transform.position, Quaternion.identity);
+		// 构建炮塔的尘土特效
+		GameObject effect = GameObject.Instantiate(buildEffect, transform.position, Quaternion.identity);
+		Destroy(effect, 1);
 	}
 }
