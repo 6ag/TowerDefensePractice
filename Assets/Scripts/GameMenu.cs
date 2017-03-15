4fe1fdb7d7e61096f4bf6900ddfa8a8897bc6670
zhoujianfeng <admin@6ag.cn>
2017-03-15 09:53:59 +0800

    完善游戏开场菜单场景

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour {
+
+	// 开始游戏
+	public void OnStartGame()
+	{
+		// 加载编号为1的场景
+		SceneManager.LoadScene(1);
+	}
+
+	// 退出游戏
+	public void OnExitGame()
+	{
+		// 退出游戏
+		#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+		#else
+			Application.Quit();
+		#endif
+	}
+
+}
